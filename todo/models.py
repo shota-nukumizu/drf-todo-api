bd4b5fea7f9bf056d8fd91a926a1eab77e2346b1
shota-nukumizu <author2000.1225@gmail.com>
2022-07-19 14:21:22 +0900

    make urls and models

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+PRIORITY_CHOICES = [
+    ('first', 'first'),
+    ('second', 'second'),
+    ('third', 'third'),
+    ('fourth', 'fourth'),
+]
+
+class Tag(models.Model):
+    word = models.CharField(max_length=40)
+
+class Todo(models.Model):
+    title = models.CharField(max_length=70)
+    description = models.TextField(blank=True)
+    priority = models.CharField(
+        max_length=6,
+        choices=PRIORITY_CHOICES
+    )
+    tags = models.ManyToManyField(Tag)
+    is_done = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
